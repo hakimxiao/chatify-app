0ad9e7250d9609cc5db8e85d309adf2197a95238
hakimxiao <alhakimabusolihin@gmail.com>
2025-12-21 23:05:11 +0700

    signup section code refactor

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,8 +1,11 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = async (userId, res) => {
+    const { JWT_SECRET } = process.env;
+    if(!JWT_SECRET) throw new Error("JWT_SECRET not defined");
+
     // buat token 
-    const token = jwt.sign({userId}, process.env.JWT_SECRET, {expiresIn: '7d'});
+    const token = jwt.sign({userId}, JWT_SECRET, {expiresIn: '7d'});
 
     res.cookie("jwt", token, {
             maxAge: 7 * 24 * 60 * 60 * 1000, // 7 hari dalam milidetik
